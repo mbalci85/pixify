a655d412971f85536da6b9ff902a3ca7136a8c85
mustafa balci <mbalci85@gmail.com>
2023-06-10 22:43:08 +0100

    theme change func added

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,13 +1,29 @@
 const themeBtn = document.querySelector('.theme-btn');
 
-const changeTheme = () => {
+const toggleTheme = () => {
 	const body = document.querySelector('body');
+	const pageTitle = document.querySelector('h1');
+	const button = document.querySelector('.about-submit-now');
+	const text = document.querySelectorAll('p');
+	console.log(6666);
 
-	if (body.style.backgroundColor == 'white') {
-		body.style.backgroundColor == '#2A3B49';
+	if (themeBtn.innerHTML === 'Dark Mode') {
+		body.style.backgroundColor = '#2A3B49';
+		pageTitle.style.color = 'white';
+		themeBtn.innerHTML = 'Light Mode';
+		for (let i = 0; i < text.length; i++) {
+			text[i].style.color = 'white';
+		}
+		button.style.border = '2px solid #7BFFE9';
 	} else {
-		body.style.backgroundColor == 'white';
+		body.style.backgroundColor = 'white';
+		pageTitle.style.color = 'black';
+		themeBtn.innerHTML = 'Dark Mode';
+		for (let i = 0; i < text.length; i++) {
+			text[i].style.color = 'black';
+		}
+		button.style.border = 'none';
 	}
 };
 
-themeBtn.onclick = changeTheme;
+themeBtn.onclick = toggleTheme;
